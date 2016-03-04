612fc7926a95842cdfafe48757f6945cc26a9d1f
Anthony Pigeot <anthony.pigeot@gmail.com>
2016-03-04 20:55:55 +0100

    Initial project setup (require.js + a few libs)

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/* Config de require.js*/
+requirejs.config ({
+	baseUrl: "js/",
+	paths: {
+
+		/* External libs */
+		Stats 			: "../libs/Stats/Stats",
+		jquery 			: "../libs/jquery/jquery.min",
+		underscore 		: "../libs/underscore/underscore",
+		howl 			: "../libs/howler/howler.min",
+		"requirejs-domready": "../libs/requirejs-domready/domReady",
+	},
+	shim: {
+		jquery: {
+			exports: "$"
+		},
+		underscore: {
+			exports: "_"
+		},
+		Stats : {
+			exports : "Stats"
+		},
+		howl : {
+			exports : "Howler",
+			init : function () {
+				return {
+					Howl : Howl,
+					Howler : Howler
+				}
+			}
+		}
+	},
+	// This is to avoid caching during development by appending the current date to script urls
+	urlArgs: "d=" + Date.now()
+});
+
+require (["requirejs-domready"],
+function (domready) {
+
+});
